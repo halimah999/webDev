8ccd8b2e3fa5d2ed93a594c1c028c80dccf46d0d
halima <studentimamu@gmail.com>
2022-10-13 12:39:05 +0300

    add all designs

--- project2/javascript.js
+++ project2/javascript.js
@@ -1,46 +1,63 @@
 
 const form = document.getElementById("form");
+const commentsRender = document.querySelector(".comments-render");
 const textarea = document.getElementById("textarea-tag");
 const user = document.getElementById("name");
-
-// let lisComments = document.querySelector(".p-info");
-
-
-let lisComments = document.querySelector(".comments-info");
-
-
-// console.log(lisComments.innerHTML);
-// console.log(form.innerHTML);
+const cloneImage = document.querySelector(".img-gender").cloneNode(true);
 
 
 
+let lisComments = document.querySelector(".comments");
+let addButton = document.getElementById("add-review");
+
+
+addButton.addEventListener("click", displayForm);
 
 form.addEventListener("submit",addComment);
 
-function addComment(e){
-    console.log("here");
-
+function displayForm(e){
     e.preventDefault();
-    let valueName = user.value;
+    commentsRender.style.display = "flex";
 
-    let valueComment = textarea.value;
-
-    let commentP = document.createElement("p");
-    let commentH6 = document.createElement("h6");
-
-    let divP = document.createElement("div");
-    let divH6 = document.createElement("div");
-
-
-    divP.classList.add("p-info");
-    divH6.classList.add("h6-info");
-    commentP.textContent = valueComment;
-    commentH6.textContent = valueName;
-    divP.append(commentP)
-    divH6.append(commentH6)
-    lisComments.append(divH6)
-    lisComments.append(divP)
-    textarea.value="";
-    user.value = "";
 
 }
+
+function addComment(e){
+    let valueName = user.value;
+    let valueComment = textarea.value;
+    e.preventDefault();
+    if(valueName.length!==0 && valueComment.length!==0){
+
+        
+
+        let elemName = document.createElement("p");
+        let elemReview = document.createElement("p");
+        let divP = document.createElement("div");
+        let divComment = document.createElement("div");
+
+
+        divComment.classList.add("comments-info")
+        divP.classList.add("user-review");
+        cloneImage.classList.add('img-gender');
+        cloneImage.setAttribute('id', lisComments.children.length)
+
+
+
+        elemName.textContent = valueName;
+        elemReview.textContent = valueComment;
+        divComment.appendChild(cloneImage);
+        divP.append(elemName);
+        divP.append(elemReview);
+        divComment.append(divP);
+
+        let position =  (lisComments.children.length)-2;
+
+        lisComments.insertBefore(divComment,lisComments.children[position]);}
+
+    textarea.value="";
+    user.value = "";
+    commentsRender.style.display = "none";
+
+}
+
+
