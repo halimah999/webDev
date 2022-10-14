c6e13f9fac559e917845be6497458254ba06655d
halima <studentimamu@gmail.com>
2022-10-14 16:12:49 +0300

    modified

--- project2/javascript.js
+++ project2/javascript.js
@@ -1,25 +1,18 @@
-
 const form = document.getElementById("form");
 const commentsRender = document.querySelector(".comments-render");
 const textarea = document.getElementById("textarea-tag");
 const user = document.getElementById("name");
-const cloneImage = document.querySelector(".img-gender").cloneNode(true);
-
-
-
 let lisComments = document.querySelector(".comments");
 let addButton = document.getElementById("add-review");
 
 
-addButton.addEventListener("click", displayForm);
 
+addButton.addEventListener("click", displayForm);
 form.addEventListener("submit",addComment);
 
 function displayForm(e){
     e.preventDefault();
     commentsRender.style.display = "flex";
-
-
 }
 
 function addComment(e){
@@ -27,37 +20,14 @@ function addComment(e){
     let valueComment = textarea.value;
     e.preventDefault();
     if(valueName.length!==0 && valueComment.length!==0){
-
-        
-
-        let elemName = document.createElement("p");
-        let elemReview = document.createElement("p");
-        let divP = document.createElement("div");
-        let divComment = document.createElement("div");
-
-
-        divComment.classList.add("comments-info")
-        divP.classList.add("user-review");
-        cloneImage.classList.add('img-gender');
-        cloneImage.setAttribute('id', lisComments.children.length)
-
-
-
-        elemName.textContent = valueName;
-        elemReview.textContent = valueComment;
-        divComment.appendChild(cloneImage);
-        divP.append(elemName);
-        divP.append(elemReview);
-        divComment.append(divP);
-
+        const cloneCommentsInfo = document.querySelector(".comments-info").cloneNode(true);
+        cloneCommentsInfo.setAttribute('id',document.getElementsByClassName('comments-info').length);
+        cloneCommentsInfo.children[1].children[0].textContent = valueName;
+        cloneCommentsInfo.children[1].children[1].textContent = valueComment;
         let position =  (lisComments.children.length)-2;
-
-        lisComments.insertBefore(divComment,lisComments.children[position]);}
+        lisComments.insertBefore(cloneCommentsInfo,lisComments.children[position]);
 
     textarea.value="";
     user.value = "";
-    commentsRender.style.display = "none";
-
+    commentsRender.style.display = "none";}
 }
-
-
